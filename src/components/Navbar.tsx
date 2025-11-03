14f4c70db8ce4bbe4d0efb64bf02591177fd337b
Barun Mahato <mbarun515@gmail.com>
2025-11-03 09:43:45 +0530

    Created some basic routes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+/* components/Navbar.tsx */
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-white shadow-md">
+      {/* This 'container mx-auto' div is what "centers" the whole thing.
+        It creates a block in the middle of the screen.
+      */}
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        
+        {/* This is the 1st item (pushed to the LEFT) */}
+        <Link href="/" className="text-2xl font-bold text-indigo-600">
+          MyPayApp
+        </Link>
+
+        {/* This is the 2nd item (pushed to the RIGHT) */}
+        <div className="flex gap-4 items-center">
+          <Link
+            href="/signup"
+            className="text-gray-600 hover:text-indigo-600 font-medium"
+          >
+            Sign Up
+          </Link>
+          <Link
+            href="/login"
+            className="bg-indigo-600 text-white px-4 py-2 rounded-md font-medium hover:bg-indigo-700"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
